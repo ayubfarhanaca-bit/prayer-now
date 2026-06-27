a873a7f8c82c33d02ac8469f3fdb95385c13f388
ayubfarhanaca-bit <ayubfarhanaca@gmail.com>
2026-06-27 09:27:37 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open('prayer-now-v1').then(cache => cache.addAll(['index.html','manifest.json'])));
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
